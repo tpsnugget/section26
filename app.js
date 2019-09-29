a795cc2b7778ab21441b1fa142a3908d69309fb3
Michael Giebner <mikegiebner@gmail.com>
2019-09-29 19:45:21 -0400

    Completed # Version 3: Colt Steele's Web Dev Bootcamp, Section 26, Chapter 274 # Serving Custom Assets

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var express = require('express')
 var app = express()
 
+// This tells Express to look in the 'public' folder if it can't find a file
+app.use(express.static('public'))
+
+// This tells Express that our files will be ejs files, so we can leave that
+// extension off in our naming
+app.set('view engine', 'ejs')
+
 // ============
 //    Routes
 // ============
 app.get('/', function(req, res) {
-   res.render('home.ejs')
+   res.render('home')
+})
+
+app.get('/fallinlovewith/:thing', function(req, res) {
+   var thing = req.params.thing
+   res.render('love', {thingVar: thing})
 })
 
 app.get('/person/:name', function(req, res) {
@@ -13,7 +25,7 @@ app.get('/person/:name', function(req, res) {
 
    //                      person is a variable in the ejs file that comes from
    //                      the :name variable in this route
-   res.render('home.ejs', {person: name,
+   res.render('name', {person: name,
                            person1: 'Margaret',
                            person2: 'Rusty'})
 })
